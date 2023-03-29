151175f8a6fe244c555c0f783fb7463c88b3075f
ValentijnVMA <9020514@student.zadkine.nl>
2023-03-29 10:51:24 +0200

    opdracht 6.3

--- opdracht 6.3/app.js
+++ opdracht 6.3/app.js
@@ -1,4 +1,5 @@
-document.getElementById("titel").style.color = "blue";
+function kleur_aanpassen() {
+    document.getElementById("titel").style.color = "blue";
 document.getElementById("titel").style.fontWeight = "bold";
 document.getElementById("para").innerText = "youri is een uitslover";
-
+}
