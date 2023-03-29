da68581e539c84e07234bd0fd1f525fbbcd4483d
ValentijnVMA <9020514@student.zadkine.nl>
2023-03-29 09:52:38 +0200

    opdracht 5.1

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("titel").style.color = "blue";
+document.getElementById("titel").style.fontWeight = "bold";
+document.getElementById("para").innerText = "youri is een uitslover";
+
